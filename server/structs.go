e071cc27c2d154885bcf401b2967bf1570333acd
Bloodcount <searosss@gmail.com>
2016-06-07 23:37:16 +0300

    app overhead

--- server/structs.go
+++ server/structs.go
@@ -1,8 +1,19 @@
 package main
 
+import "github.com/golang/glog"
+
 type note struct {
 	Payload      string
 	IsEncrypted  bool
 	ReadOnlyHash *string
 	EditHash     string
+}
+
+type app struct {
+	APIPort string
+	w       Storager
+}
+
+func (app *app) LoadSettings() {
+	glog.Info(`LoadSettings`)
 }
