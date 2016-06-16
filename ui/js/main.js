fa43e4dd7d3b7db1d3db94d3519614d778fd2606
Bloodcount <searosss@gmail.com>
2016-06-17 00:50:51 +0300

    refactor: JSLint is ok

--- ui/js/main.js
+++ ui/js/main.js
@@ -1,60 +1,47 @@
 /*jslint browser: true*/
-/*global GibberishAES, $, jQuery, alert, console*/
-
-var API = (function () {
+/*global QuickNote, GibberishAES, $, jQuery, alert, console, CKEDITOR, JsSHA*/
+function QuickNoteAPI() {
     'use strict';
+    this.Endpoint = "localhost:7001";
+}
 
-    var enc, dec, shaObj;
+QuickNote = function () {
+    /*
     enc = GibberishAES.enc("This sentence is super secret", "ultra-strong-password");
     console.log(enc);
     dec = GibberishAES.dec(enc, "ultra-strong-password");
     console.log(dec);
 
-    shaObj = new jsSHA("SHA-256", "TEXT");
+    shaObj = new JsSHA("SHA-256", "TEXT");
     console.log(shaObj.getHash("HEX"));
-    var QuickNote = function () {
-        var editorElementId = "quick-note-editor";
-        var apiEndpoint = "";
-        var self = this;
 
-    Load = function(){
-        CKEDITOR.replace( this.editorElementId);
+    console.log("this: ", this);*/
+    'use strict';
+
+    this.Load = function () {
+        CKEDITOR.replace('quick-note-editor');
         CKEDITOR.config.height = '400';   // CSS unit (percent).
         CKEDITOR.instances[this.editorElementId].setData('Start typing your note here');
 
         jQuery('#saveBtn').click(this.SaveNote);
     };
 
-    DisplayNote = function(){
+    this.DisplayNote = function () {
         console.log("DisplayNote!!" + window.location.pathname);
-
     };
 
-    SaveNote = function(){
+    this.SaveNote = function () {
         console.log("SaveNote!!!!!1");
         console.log("this: ", this);
-        console.log("self: ", self);
-        self.EncryptNote();
     };
 
-    EncryptNote = function(){
+    this.EncryptNote = function () {
         console.log("EncryptNote!");
-        console.log("QuickNoteAPI: ", API.Endpoint)
+        console.log("QuickNoteAPI: ", API.Endpoint);
     };
 
-    GenerateHashPass = function(){
-
+    this.GenerateHashPass = function () {
+        console.log("foobar");
     };
-    API = new QuickNoteAPI();
+    this.API = new QuickNoteAPI();
 };
-
-function QuickNoteAPI(){
-    this.Endpoint = "localhost:7001";
-
-}
-
-
-console.log(QuickNote);
-QuickNote.Load();
-
-}());
