7243ae8c4ab93458377bdb2fe093e7b8b03663ba
Bloodcount <searosss@gmail.com>
2016-07-05 19:51:26 +0300

    Implements in memory storage

--- server/storage_memory.go
+++ server/storage_memory.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"errors"
+
+	"github.com/golang/glog"
+)
+
+type note struct {
+	Payload      string
+	IsEncrypted  bool
+	ReadOnlyHash string
+	EditHash     string
+}
+
 func NewWriterMemory() WriterMemory {
 	var wm WriterMemory
 	wm.notesEdit = make(map[string]note)
@@ -13,15 +26,38 @@ type WriterMemory struct {
 }
 
 func (w *WriterMemory) getNote(readOnlyHash *string, editHash *string) (n note, err error) {
+	glog.Info("BEFORE NOTES TO EDIT")
+	if readOnlyHash != nil && editHash != nil {
+		return n, errors.New("Only one argument can be defined.")
+	}
 
-	return n, err
+	glog.Info("PRINTING THE NOTES TO EDIT", w.notesEdit)
+	glog.Info("PRINTING THE READ ONLY NOTES", w.notesReadOnly)
+
+	glog.Info("AFTER NOTES TO EDIT")
+	if readOnlyHash != nil && *readOnlyHash != "" {
+		val, exists := w.notesReadOnly[*readOnlyHash]
+		if !exists {
+			return val, errors.New(`Key with readOnlyHash: ` + *readOnlyHash + ` doesn't exist.`)
+		}
+		return val, err
+	} else if editHash != nil && *editHash != "" {
+		val, exists := w.notesEdit[*editHash]
+		if !exists {
+			return val, errors.New(`Key with editHash: ` + *editHash + ` doesn't exist.`)
+		}
+		return val, err
+	}
+	return n, errors.New("System error.")
 }
 
 func (w *WriterMemory) setNote(newNote note) (n note, err error) {
 	w.notesEdit[newNote.EditHash] = newNote
-	if newNote.ReadOnlyHash != nil {
-		w.notesReadOnly[*newNote.ReadOnlyHash] = newNote
+	if newNote.ReadOnlyHash != `` {
+		w.notesReadOnly[newNote.ReadOnlyHash] = newNote
 	}
 
+	glog.Info("PRINTING THE NOTES TO EDIT", w.notesEdit)
+	glog.Info("PRINTING THE READ ONLY NOTES", w.notesReadOnly)
 	return n, err
 }
