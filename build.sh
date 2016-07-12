c61766f81dafc591d350f1cf9ff9339d2713feda
Bloodcount <searosss@gmail.com>
2016-07-12 22:43:04 +0300

    feat: parametrizes the report

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 cd server
+
 $GOROOT/bin/go test -coverprofile=c.out -logtostderr=true
-#$GOROOT/bin/go tool cover -html=c.out
+if [ "$1" == "report" ]; then
+    $GOROOT/bin/go tool cover -html=c.out
+fi
 $GOROOT/bin/go build
 
 cd ..
